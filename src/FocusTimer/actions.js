85d5f600867c55bcca299ed03ba00829c17afa94
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-09-03 17:48:28 -0300

    feat: added countdown structure

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,7 +1,10 @@
 import state from './state.js';
+import * as timer from './timer.js'
 
 export function toggleRunning() {
   state.isRunning = document.documentElement.classList.toggle('running');
+
+  timer.countdown()
 }
 
 export function reset() {
